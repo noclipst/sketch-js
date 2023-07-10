782fdeea647bcbc56c210af8b7037297099751bb
noclipst <noclipst@gmail.com>
2023-07-10 22:21:30 +0300

    Implement further functionality, still WIP

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
-const sketchboard = document.getElementById('sketchboard');
+// TODO: show/hide pixel borders
 
-generateBoard();
+const sketchboard = document.getElementById('sketchboard');
+const boardSizeSlider = document.getElementById('board-size-slider');
+const boardSizeText = document.getElementById('board-size-slider-text');
+
+generateBoard(256); // default value, 16x16
 
 const pixels = document.querySelectorAll('.pixel');
 
+// ---  event listeners  ---
+
 pixels.forEach(pixel => pixel.addEventListener('mouseover', () => changePixelColor(pixel)));
 
-function generateBoard() {
-    for (i = 0; i < 256; i += 1) {
+boardSizeSlider.addEventListener("input", (e) => {
+    const sliderValue = e.target.value;
+
+    clearBoard();
+    generateBoard(sliderValue * sliderValue);
+    boardSizeText.textContent = `Grid size: ${sliderValue} x ${sliderValue}`;
+    resizePixels(sliderValue);
+    updatePixelEventListeners(); // once the board is regenerated the mouseover event listener no longer works; needs updating
+})
+
+// -------------------------
+
+function generateBoard(size) {
+    for (i = 0; i < size; i += 1) {
         const div = document.createElement('div');
         div.className = 'pixel';
         div.id = `pixel-${i}`;
@@ -19,4 +37,25 @@ function generateBoard() {
 function changePixelColor(pixel) {
     pixel.style.backgroundColor = "black";
     console.log(`Changing color of pixel to black`);
+}
+
+function clearBoard() {
+    while (sketchboard.firstChild) {
+        sketchboard.removeChild(sketchboard.firstChild);
+    }
+}
+
+function updatePixelEventListeners() {
+    const pixels = document.querySelectorAll('.pixel');
+
+    pixels.forEach(pixel => pixel.addEventListener('mouseover', () => changePixelColor(pixel)));
+}
+
+function resizePixels(boardSize) {
+    const pixelDiv = document.querySelectorAll('.pixel');
+    const sketchboardDivWidth = 450; 
+
+    const pixelWidth = sketchboardDivWidth / boardSize;
+    
+    pixelDiv.forEach(pixel => pixel.style.padding = pixelWidth);
 }
