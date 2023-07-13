dddef65d3894c5843324db53fbadd700cc5babe2
noclipst <noclipst@gmail.com>
2023-07-14 01:33:59 +0300

    Add show grid button and related functionality

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-// TODO: show/hide pixel borders
-
 const sketchboard = document.getElementById('sketchboard');
 const boardSizeSlider = document.getElementById('board-size-slider');
 const boardSizeText = document.getElementById('board-size-slider-text');
+const showGridCheckbox = document.getElementById('show-grid-checkbox');
+
 const boardWidth = 500; // board width in pixels
-generateBoard(256, 31.25); // default board size (grid 16x16) and pixel width and height for that size
+generateBoard(256, 31.25); // default board size (16x16 grid) and pixel width and height for that size
 
 const pixels = document.querySelectorAll('.pixel');
 
@@ -18,7 +18,21 @@ boardSizeSlider.addEventListener("input", (e) => {
     clearBoard();
     generateBoard((sliderValue * sliderValue), (boardWidth / sliderValue)); // the first parameter is the total board size, the second is width and height of each pixel for the given slider value
     boardSizeText.textContent = `Grid size: ${sliderValue} x ${sliderValue}`;
-    updatePixelEventListeners(); // once the board is regenerated the mouseover event listener no longer works; needs updating
+    updatePixelEventListeners(); // once the board is regenerated all the pixel event listeners stop working, need updating
+})
+
+showGridCheckbox.addEventListener("input", function() {
+    if (this.checked) {
+        pixels.forEach(pixel => {
+            pixel.classList.add('pixel');
+            pixel.classList.remove('pixel_nogrid');
+        });
+    } else {
+        pixels.forEach(pixel => {
+            pixel.classList.remove('pixel');
+            pixel.classList.add('pixel_nogrid');
+        });
+    }
 })
 
 // -------------------------
@@ -51,4 +65,18 @@ function updatePixelEventListeners() {
     const pixels = document.querySelectorAll('.pixel');
 
     pixels.forEach(pixel => pixel.addEventListener('mouseover', () => changePixelColor(pixel)));
+
+    showGridCheckbox.addEventListener("input", function() {
+        if (this.checked) {
+            pixels.forEach(pixel => {
+                pixel.classList.add('pixel');
+                pixel.classList.remove('pixel_nogrid');
+            });
+        } else {
+            pixels.forEach(pixel => {
+                pixel.classList.remove('pixel');
+                pixel.classList.add('pixel_nogrid');
+            });
+        }
+    })
 }
